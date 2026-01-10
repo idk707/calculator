802185fed1ca06d39d1b49c84ba0a1107eff79fb
idk707 <156857232+idk707@users.noreply.github.com>
2026-01-10 16:10:19 +0000

    Work In Progress

--- index.py
+++ index.py
@@ -5,4 +5,26 @@ app = Flask(__name__)
 @app.route("/calculate", methods=["POST"])
 
 def calculate():
-    return
+    data = request.get_json()
+
+    a = int(data("a")
+    b = int(data("b")
+    op = data("op")
+
+    if op == "+":
+        result = a + b
+    elif op == "-":
+        result = a - b
+    elif op == "*":
+        result = a * b
+    elif op == "/":
+        if b == 0:
+            return jsonify({"error": "Division by zero is not allowed"}), 400
+        result = a / b
+    else:
+        return jsonify({"error": "Invalid operation"}), 400
+
+    return jsonify({"result": result})
+
+if __name__ == "__main__":
+    app.run(debug=True)
